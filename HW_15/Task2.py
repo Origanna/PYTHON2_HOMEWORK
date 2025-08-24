258337bfd417939de057cf7e4874929913b78224
Olga <origanna@yandex.ru>
2025-08-24 08:43:15 +0300

    Add HW_15

---
+++ HW_15/Task2.py
@@ -0,0 +1,21 @@
+# Задание 2.
+
+from datetime import datetime
+
+def display_current_datetime():
+    # Получение текущего времени и даты
+    now = datetime.now()
+
+    # Форматирование даты и времени
+    formatted_date = now.strftime('%Y-%m-%d %H:%M:%S')
+
+    # Получение дня недели и номера недели
+    day_of_week = now.strftime('%A')
+    week_number = now.isocalendar()[1]
+    
+    print(f'Current date and time: {formatted_date}')
+    print(f'Day of the week: {day_of_week}')
+    print(f'Week number: {week_number}')
+
+if __name__ == '__main__':
+    display_current_datetime()
